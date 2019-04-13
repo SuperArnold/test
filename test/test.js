24e45359517b8d9875f6e0f63966ed81a39fedfb
SuperArnold <ji394vul35520@hotmail.com>
2019-04-13 23:27:52 +0800

    test unit test
    use faile test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
         console.log("XXXXXX :" + [1,2,3].indexOf(2))
-      assert.equal(1, [1,2,3].indexOf(2));
+      assert.equal(1, [1,2,3].indexOf(0));
     })
   })
 })
